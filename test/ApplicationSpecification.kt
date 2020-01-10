b96288393118558a7c02f8817d60df505e51223b
jaroslawmichalik <michalik@protonmail.ch>
2020-01-10 21:06:15 +0100

    Add tests

--- test/ApplicationSpecification.kt
+++ test/ApplicationSpecification.kt
@@ -1,20 +1,20 @@
 package tech.michalik
 
+import io.kotlintest.specs.StringSpec
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
-import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class ApplicationTest {
-    @Test
-    fun testRoot() {
+class ApplicationSpecification : StringSpec({
+    "root should return plain text"{
         withTestApplication({ module() }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("HELLO WORLD!", response.content)
+                assertEquals("text/plain; charset=UTF-8", response.headers["Content-Type"])
             }
         }
     }
-}
+})
