d8267a6dae0b2831a01f258954b890025909dacb
jaroslawmichalik <michalik@protonmail.ch>
2020-07-29 10:30:43 +0200

    Refactor test to be parameterized

--- test/ApplicationSpecification.kt
+++ test/ApplicationSpecification.kt
@@ -1,19 +1,29 @@
 package tech.michalik
 
+import io.kotlintest.assertSoftly
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
-import kotlin.test.assertEquals
 
 class ApplicationSpecification : StringSpec({
-    "root should return plain text"{
-        withTestApplication({ module() }) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
-                assertEquals("text/plain; charset=UTF-8", response.headers["Content-Type"])
+    val testParams = mapOf(
+        "/json" to "application/json; charset=UTF-8",
+        "/" to "text/plain; charset=UTF-8",
+        "/html" to "text/html; charset=UTF-8"
+    )
+
+    testParams.forEach { (endpoint, expectedContentType) ->
+        "GET on $endpoint should return success and $expectedContentType"{
+            withTestApplication({ module() }) {
+                handleRequest(HttpMethod.Get, endpoint).apply {
+                    assertSoftly {
+                        response.status() shouldBe HttpStatusCode.OK
+                        response.headers["Content-Type"] shouldBe expectedContentType
+                    }
+                }
             }
         }
     }
